b61f63979c0ea1332b0d9c5a80fde9aecfe39cd8
Leftfish <41051287+Leftfish@users.noreply.github.com>
2022-12-13 18:56:01 +0100

    Add Python solution to Day 13 part 2.

--- 13/d13.py
+++ 13/d13.py
@@ -1,7 +1,11 @@
-from json import loads
+from functools import cmp_to_key, reduce
 from itertools import zip_longest
+from json import loads
+from operator import mul
 
 print('Day 13 of Advent of Code!')
+
+DIVIDERS = [[[2]], [[6]]]
  
 def compare(a, b):
     if isinstance(a, int) and isinstance(b, int):
@@ -38,8 +42,23 @@ def get_pairs(raw_data):
         pairs.append((loads(data[0]),loads(data[1])))
     return pairs
 
-def sum_proper(pairs):
-    return sum(i + 1 for i, pair in enumerate(pairs) if compare(*pair))
+def sum_proper_packets(pairs):
+    return sum(idx for idx, pair in enumerate(pairs, start=1) if compare(*pair))
+
+def get_decoder_key(pairs):
+    def comparator(a, b):
+        if compare(a, b):
+            return 1
+        elif not compare(a, b):
+            return -1
+        return 0
+    
+    elements = [*DIVIDERS]
+    for pair in pairs:
+        elements.extend(list(pair))
+    
+    indices = [idx for idx, packet in enumerate(sorted(elements, key=cmp_to_key(comparator), reverse=True), start=1) if packet in DIVIDERS]
+    return reduce(mul, indices, 1)
 
 test_data = '''[1,1,3,1,1]
 [1,1,5,1,1]
@@ -67,9 +86,11 @@ test_data = '''[1,1,3,1,1]
 
 print('Testing...')
 pairs = get_pairs(test_data)
-print('Sum of indices of proper pairs:', sum_proper(pairs) == 13)
+print('Sum of indices of proper pairs:', sum_proper_packets(pairs) == 13)
+print('Decoder key:', get_decoder_key(pairs) == 140)
 
 with open('inp', mode='r') as inp:
     print('Solution...')
     pairs = get_pairs(inp.read())
-    print('Sum of indices of proper pairs:', sum_proper(pairs))
+    print('Sum of indices of proper pairs:', sum_proper_packets(pairs))
+    print('Decoder key:', get_decoder_key(pairs))
